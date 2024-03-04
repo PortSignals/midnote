ebce6caac5b1444904f9f62ff94e5ade2b2e82f7
lilith <hello@justlilith.com>
2024-03-04 03:26:56 -0600

    update deps -L6

---
+++ src/components/midnote.svelte
@@ -0,0 +1,48 @@
+<script lang='ts'>
+  import { slide } from 'svelte/transition';
+  
+  import { onMount } from 'svelte';
+  
+  let visible = false
+  
+  onMount(()=> {
+  })
+</script>
+
+<div class="expandable">
+  <div class="expandable-label">
+    <button class="expandable-button" on:click="{()=> {visible = !visible}}"><slot name="label"></slot></button>
+  </div>
+  {#if visible}
+  <section class="expandable-content" transition:slide>
+    <slot name="content"></slot>
+  </section>
+{/if}
+</div>
+
+<style lang='css'>
+  .expandable {
+    margin: 0;
+  }
+  .expandable-label {
+    background: black;
+    color: hsl(0,0%,70%);
+    border-left: medium solid cyan;
+    padding-left:12px;
+    border-bottom: thin solid hsl(0,0%,15%);
+  }
+  .expandable-button {
+    background: none;
+    border: none;
+    color: hsl(0,0%,70%);
+    cursor: pointer;
+    font-size: 0.9em;
+    margin: none;
+    padding: none;
+  }
+  .expandable-content {
+    background: hsl(0,0%,20%);
+    color: white;
+    padding:12px;
+  }
+</style>
